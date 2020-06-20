c11688d23dabfe3727b6cf40c574e9e850d93743
Sergii Bala <sergiibala@gmail.com>
2020-06-20 05:42:33 -0400

    Initial Commit

---
+++ UDDER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UDDER
+//
+//  Created by Sergii Bala on 6/20/20.
+//  Copyright © 2020 Sergii Bala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
